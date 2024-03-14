2e2bafa800b47d6fd01d91ec389a0114efa13a15
Thamiris Maria <thamirismariasouzadeoliveira@gmail.com>
2024-03-14 17:33:15 -0300

    feat: class Musica

---
+++ Musica.cs
@@ -0,0 +1,26 @@
+﻿namespace ScreenSound
+{
+    public class Musica
+    {
+        public string Nome { get; set; }
+        public string Artista { get; set; }
+        public int Duracao { get; set; }
+        public bool Disponivel { get; set; }
+
+        public Genero Genero { get; set; }
+        public string DescricaoResumida => $"A música {Nome} pertence à banda {Artista}";
+
+        public void ExibirFichaTecnica()
+        {
+            Console.WriteLine($"Nome: {Nome}\n" +
+                $"Artista: {Artista}\n" +
+                $"Duracao: {Duracao}\n" +
+                $"{(Disponivel ? "Disponível no plano" : "Assine o plano Plus")}");
+        }
+
+        public void ExibirNomeEArtista()
+        {
+            Console.WriteLine($"Nome/Artista: {Nome} - {Artista}");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using ScreenSound;
+
+Musica musica1 = new Musica();
+musica1.Nome = "Roxane";
+musica1.Artista = "The Police";
+musica1.Duracao = 273;
+musica1.Disponivel = true;
+
+Musica musica2 = new Musica();
+musica2.Nome = "Vertigo";
+musica2.Artista = "US";
+musica2.Duracao = 967;
+musica2.Disponivel = false;
+
+musica1.ExibirFichaTecnica();
+musica2.ExibirFichaTecnica();
