42d84007451260e287f2273485989129edfabe7c
Thamiris Maria <thamirismariasouzadeoliveira@gmail.com>
2024-03-15 11:41:16 -0300

    feat: classes Podcast e Episodio

---
+++ Episodio.cs
@@ -0,0 +1,27 @@
+﻿namespace ScreenSound
+{
+    public class Episodio
+    {
+        public Episodio(string titulo, int ordem, int duracao)
+        {
+            Titulo = titulo;
+            Ordem = ordem;
+            Duracao = duracao;
+        }
+
+        public string Titulo { get;}
+        public int Ordem { get;}
+        public int Duracao { get;}
+
+        private List<string> convidados = new();
+        public string Resumo => $"{Ordem}. {Titulo} ({Duracao} min) - " +
+            $"{string.Join(", ", convidados)}";
+
+        public void AdicionarConvidados(string convidado)
+        {
+            convidados.Add(convidado);
+        }
+
+
+    }
+}
---
+++ Podcast.cs
@@ -0,0 +1,31 @@
+﻿namespace ScreenSound
+{
+    class Podcast
+    {
+        public string Host { get; set; }
+        public string Nome { get; set; }
+        private List<Episodio> episodios = new List<Episodio>();
+        public int TotalEpisodios => episodios.Count();
+
+        public Podcast(string host, string nome)
+        {
+            Host = host;
+            Nome = nome;
+        }
+
+        public void AdicionarEpisodio(Episodio episodio)
+        {
+            episodios.Add(episodio);
+        }
+
+        public void ExibirDetalhes()
+        {
+            Console.WriteLine($"Podcast {Nome} apresentado por {Host}\n");
+            foreach (Episodio episodio in episodios.OrderBy(episodio => episodio.Ordem))
+            {
+                Console.WriteLine(episodio.Resumo);
+            }
+            Console.WriteLine($"\nTodal de episodios: {TotalEpisodios}");
+        }
+    }
+}
